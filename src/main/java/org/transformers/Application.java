c7181f23c086bbae2ef4d4cda52dc3e96591fac0
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-13 15:34:01 +0100

    Initialization

---
+++ src/main/java/org/transformers/Application.java
@@ -0,0 +1,14 @@
+package org.transformers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//TODO Add tests
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/org/transformers/ApplicationShould.java
@@ -0,0 +1,16 @@
+package org.transformers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationShould {
+    @Test
+    public void contextLoads() {
+    }
+
+    //TODO Add tests
+}
