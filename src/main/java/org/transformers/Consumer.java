3745ed6349ac19b85265a23ddec400147e3f583e
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-14 09:34:07 +0100

    Switched to temperature topic

--- src/main/java/org/transformers/Consumer.java
+++ src/main/java/org/transformers/Consumer.java
@@ -3,6 +3,7 @@ package org.transformers;
 import io.confluent.developer.User;
 import lombok.extern.apachecommons.CommonsLog;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.opensmartgridplatform.adapter.kafka.MeterReading;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,10 @@ public class Consumer {
   @Value("${topic.name}")
   private String topicName;
 
-  @KafkaListener(topics = "users", groupId = "group_id")
-  public void consume(ConsumerRecord<String, User> record) {
+  @KafkaListener(topics = "meter-reading-internal-temperature", groupId = "group_id")
+  public void consume(ConsumerRecord<String, MeterReading> record) {
     System.out.println(String.format("Consumed message -> %s", record.value()));
     //log.info(String.format("Consumed message -> %s", record.value()));
   }
+
 }
