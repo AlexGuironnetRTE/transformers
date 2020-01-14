990f55c287b7ec617b7b75d4bc9c8612bc6ff059
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-14 09:11:31 +0100

    Attempt following Confluent tutorial

--- src/main/java/org/transformers/Application.java
+++ src/main/java/org/transformers/Application.java
@@ -1,14 +1,31 @@
 package org.transformers;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //TODO Add tests
 
 @SpringBootApplication
 public class Application {
 
+	@Value("${topic.name}")
+	private String topicName;
+
+	@Value("${topic.partitions-num}")
+	private Integer partitions;
+
+	@Value("${topic.replication-factor}")
+	private short replicationFactor;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	NewTopic moviesTopic() {
+		return new NewTopic(topicName, partitions, replicationFactor);
 	}
 }
