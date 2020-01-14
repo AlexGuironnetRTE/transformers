07d31b7d425350a879563610bbfa9a4ed1a53a75
Alexandra Guironnet <alexandra.guironnet@rte-france.com>
2020-01-14 10:19:52 +0100

    Added card publisher and bundle

--- src/main/java/org/transformers/Consumer.java
+++ src/main/java/org/transformers/Consumer.java
@@ -3,10 +3,16 @@ package org.transformers;
 import io.confluent.developer.User;
 import lombok.extern.apachecommons.CommonsLog;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.lfenergy.operatorfabric.cards.model.Card;
+import org.lfenergy.operatorfabric.cards.model.CardCreationReport;
 import org.opensmartgridplatform.adapter.kafka.MeterReading;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.transformers.operatorfabric.CardPublisher;
+
+//TODO Make logs work (Slf4j)
 
 @Service
 //@CommonsLog(topic = "Consumer Logger")
@@ -15,9 +21,23 @@ public class Consumer {
   @Value("${topic.name}")
   private String topicName;
 
+  @Autowired
+  CardPublisher cardPublisher;
+
   @KafkaListener(topics = "meter-reading-internal-temperature", groupId = "group_id")
   public void consume(ConsumerRecord<String, MeterReading> record) {
     System.out.println(String.format("Consumed message -> %s", record.value()));
+
+    MeterReading meterReading = record.value();
+
+    Card card = cardPublisher.createMeterReadingCard(meterReading);
+
+    System.out.println("Created card: "+card.toString());
+
+    CardCreationReport report = cardPublisher.publishCard(card);
+
+    System.out.println("Card publication report: "+report.toString());
+
     //log.info(String.format("Consumed message -> %s", record.value()));
   }
 
---
+++ src/main/java/org/transformers/operatorfabric/CardPublisher.java
@@ -0,0 +1,84 @@
+package org.transformers.operatorfabric;
+
+import org.lfenergy.operatorfabric.cards.model.*;
+import org.opensmartgridplatform.adapter.kafka.MeterReading;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.HashMap;
+
+@Service
+public class CardPublisher {
+
+    public Card createMeterReadingCard(MeterReading meterReading) {
+
+        Card card = new Card();
+
+        String process = "current_meter_reading";
+
+        card.setProcess(process);
+        card.setProcessId(process+meterReading.getName());
+        card.setPublisher("TRANSFORMERS");
+        card.setPublisherVersion("1");
+
+        //TODO Get dates from the card
+        card.setLttd(Instant.now().toEpochMilli());
+        card.setStartDate(Instant.now().toEpochMilli());
+        card.setEndDate(Instant.now().toEpochMilli() + 2*60*60*1000);
+
+        card.setSeverity(SeverityEnum.NOTIFICATION);
+
+        Recipient adminUser = new Recipient();
+        adminUser.setType(RecipientEnum.USER);
+        adminUser.setIdentity("admin");
+        card.setRecipient(adminUser);
+
+        I18n i18nTitle = new I18n();
+        i18nTitle.setKey(process+".title");
+        HashMap<String,String> i18nTitleParams = new HashMap<>();
+        i18nTitleParams.put("value",meterReading.getUsagePoint().getMRid());
+        i18nTitle.setParameters(i18nTitleParams);
+        card.setTitle(i18nTitle);
+
+        I18n i18nSummary = new I18n();
+        i18nSummary.setKey(process+".summary");
+        HashMap<String,String> i18nSummaryParams = new HashMap<>();
+        i18nSummaryParams.put("value",meterReading.getIntervalBlocks().get(0).getReadingType().getMeasuringKind()); //TODO Add interval as well
+        i18nSummary.setParameters(i18nSummaryParams);
+        card.setSummary(i18nSummary);
+
+        card.setState("firstState");
+
+        card.setData(meterReading.getIntervalBlocks().get(0).getIntervalReadings().get(0));
+
+        return card;
+    }
+
+    public CardCreationReport publishCard(Card card) {
+
+        //TODO Put it in application.yml?
+        String uri = "http://localhost:2102/cards";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        /*
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");*/
+
+        HttpEntity<Card> request = new HttpEntity<>(card, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        CardCreationReport result = restTemplate.postForObject(uri, request, CardCreationReport.class);
+
+        return result;
+
+    }
+
+}
